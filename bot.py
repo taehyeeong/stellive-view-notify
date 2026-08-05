25a171ec2a8bbe11a5bb01c0a9344207b55323b1
taehyeeong <xo_hhh@icloud.com>
2026-08-05 22:18:01 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
-from telegram import Update
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -14,8 +18,35 @@ TELEGRAM_TOKEN = os.environ.get(
 
 async def start(update: Update, context):
 
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "🌸 에버리스",
+                callback_data="unit_everies"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "☁️ 유니버스",
+                callback_data="unit_universe"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "✨ 클리셰",
+                callback_data="unit_cliche"
+            )
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(
+        keyboard
+    )
+
     await update.message.reply_text(
-        "🎤 YouTube Notify 봇입니다!"
+        "🎤 YouTube Notify 봇입니다!\n\n"
+        "유닛을 선택하세요.",
+        reply_markup=reply_markup
     )
 
 
