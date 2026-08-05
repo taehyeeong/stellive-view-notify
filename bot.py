daec86c87f789bdeeb8e1ca56dd80fe4ab92ffc9
taehyeeong <xo_hhh@icloud.com>
2026-08-05 21:46:27 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+)
+
+from config import TELEGRAM_TOKEN
+
+
+async def start(update: Update, context):
+
+    await update.message.reply_text(
+        "🎤 YouTube Notify 봇입니다!"
+    )
+
+
+def main():
+
+    app = Application.builder() \
+        .token(TELEGRAM_TOKEN) \
+        .build()
+
+
+    app.add_handler(
+        CommandHandler(
+            "start",
+            start
+        )
+    )
+
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
