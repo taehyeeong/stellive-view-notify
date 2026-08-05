8cf4382273c21db1369bc73a25513c6db3327984
taehyeeong <xo_hhh@icloud.com>
2026-08-05 12:32:07 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,17 +2,7 @@
 
 # YouTube 채널 핸들
 CHANNELS = [
-    "@shirayukihina",
-    "@stellive_official",
-    "@ayatsunoyuni",
-    "＠Sakihanechannel",
-    "@neneko_mashiro",
-    "@akanelize",
-    "@arahashitabi",
-    "@tenkoshibuki",
-    "@aokumorin",
-    "@hanako_nana",
-    "@yuzuhariko"
+    "@shirayukihina"
 ]
 
 # 조회수 알림 단위
