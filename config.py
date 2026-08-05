579e3409fd6d78f0b1c602a2a5a74ed1bad4b807
taehyeeong <xo_hhh@icloud.com>
2026-08-05 11:38:15 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,37 @@
+# 감시할 유튜브 채널 설정 파일
+
+# YouTube 채널 핸들
+CHANNELS = [
+    "@shirayukihina",
+]
+
+# 조회수 알림 단위
+# 50000 = 5만 조회수마다 알림
+VIEW_STEP = 50000
+
+
+# 음악 영상으로 판단할 제목 키워드
+MUSIC_KEYWORDS = [
+    "MV",
+    "Music Video",
+    "Official",
+    "Original",
+    "Cover",
+    "歌ってみた",
+    "オリジナル",
+    "노래",
+    "cover",
+    "music",
+    "Playlist",
+    "Live"
+]
+
+
+# 특별 알림 기준
+MILESTONES = [
+    100000,     # 10만
+    500000,     # 50만
+    1000000,    # 100만
+    5000000,    # 500만
+    10000000,   # 1000만
+]
