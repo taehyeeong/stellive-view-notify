af8d7e88363e53c5c49d69e4161333447753b411
taehyeeong <xo_hhh@icloud.com>
2026-08-06 11:45:57 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,50 @@
 
 
 
+# 조회수 알림 단위
+# 50000 = 5만 조회수마다 알림
+VIEW_STEP = 50000
+
+
+# 음악 영상으로 판단할 제목 키워드
+MUSIC_KEYWORDS = [
+    "MV",
+    "Music Video",
+    "Official",
+    "Original",
+    "Cover",
+    "歌ってみた",
+    "オリジナル",
+    "노래",
+    "cover",
+    "music",
+    "Playlist",
+    "Live"
+]
+
+EXCLUDE_KEYWORDS = [
+    "shorts",
+    "#shorts",
+    "쇼츠",
+    "clip",
+    "클립",
+    "雑談",
+    "잡담"
+]
+
+
+# 특별 알림 기준
+MILESTONES = [
+    100000,     # 10만
+    500000,     # 50만
+    1000000,    # 100만
+    5000000,    # 500만
+    10000000,   # 1000만
+]
+
+
+===========================
+
 # 조회수 달성 축하 메시지 템플릿
 
 MILESTONE_TEMPLATE = """
@@ -152,46 +196,6 @@ UNITS = {
     }
 }
 
-# 조회수 알림 단위
-# 50000 = 5만 조회수마다 알림
-VIEW_STEP = 50000
-
-
-# 음악 영상으로 판단할 제목 키워드
-MUSIC_KEYWORDS = [
-    "MV",
-    "Music Video",
-    "Official",
-    "Original",
-    "Cover",
-    "歌ってみた",
-    "オリジナル",
-    "노래",
-    "cover",
-    "music",
-    "Playlist",
-    "Live"
-]
-
-EXCLUDE_KEYWORDS = [
-    "shorts",
-    "#shorts",
-    "쇼츠",
-    "clip",
-    "클립",
-    "雑談",
-    "잡담"
-]
-
-
-# 특별 알림 기준
-MILESTONES = [
-    100000,     # 10만
-    500000,     # 50만
-    1000000,    # 100만
-    5000000,    # 500만
-    10000000,   # 1000만
-]
 
 
 INITIAL_SETUP = False
